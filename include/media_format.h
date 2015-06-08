e47df412b77bcda8615dbcff056e2b65d764c478
jihae.yi <jihae.yi@samsung.com>
2015-06-08 17:02:57 +0900

    modify doxygen description
    Change-Id: I96a2849dd942016da93332b920b3c5bb91aff07c

--- include/media_format.h
+++ include/media_format.h
@@ -20,6 +20,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <tizen.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -81,31 +82,26 @@ typedef enum {
  * @brief Enumeration for media format MIME type.
  * @since_tizen 2.3
  */
-
-/* TODO : More an more audio/video types are needed !!!! */
-/* TODO : consider to make those enumerations into a combination of bitwised structure
- * Then we can check just few bits rather than compare all items.
-*/
 typedef enum {
     /* Audio */
     MEDIA_FORMAT_L16 = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1010),           /**< media format mime type is L16, AUDIO*/
     MEDIA_FORMAT_ALAW = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1020),          /**< media format mime type is ALAW, AUDIO*/
     MEDIA_FORMAT_ULAW = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1030),          /**< media format mime type is ULAW,  AUDIO */
-    MEDIA_FORMAT_AMR = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1040),           /**< media format mime type is AMR,  AUDIO,  indicates MEDIA_FORMAT_AMR_NB (Since tizen 2.4) */
-    MEDIA_FORMAT_AMR_NB = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1040),        /**< media format mime type is AMR_NB,  AUDIO , (Since tizen 2.4) */
-    MEDIA_FORMAT_AMR_WB = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1041),        /**< media format mime type is AMR_WB,  AUDIO, (Since tizen 2.4) */
+    MEDIA_FORMAT_AMR = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1040),           /**< media format mime type is AMR,  AUDIO,  indicates MEDIA_FORMAT_AMR_NB (Since 2.4) */
+    MEDIA_FORMAT_AMR_NB = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1040),        /**< media format mime type is AMR_NB,  AUDIO , (Since 2.4) */
+    MEDIA_FORMAT_AMR_WB = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1041),        /**< media format mime type is AMR_WB,  AUDIO, (Since 2.4) */
     MEDIA_FORMAT_G729 = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1050),          /**< media format mime type is G729,  AUDIO*/
-    MEDIA_FORMAT_AAC = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1060),           /**< media format mime type is AAC,  AUDIO, indicates MEDIA_FORMAT_AAC_LC (Since tizen 2.4) */
-    MEDIA_FORMAT_AAC_LC = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1060),        /**< media format mime type is AAC_LC,  AUDIO, (Since tizen 2.4) */
-    MEDIA_FORMAT_AAC_HE = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1061),        /**< media format mime type is AAC_HE,  AUDIO, (Since tizen 2.4) */
-    MEDIA_FORMAT_AAC_HE_PS = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1062),     /**< media format mime type is AAC_HE_PS,  AUDIO, (Since tizen 2.4) */
+    MEDIA_FORMAT_AAC = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1060),           /**< media format mime type is AAC,  AUDIO, indicates MEDIA_FORMAT_AAC_LC (Since 2.4) */
+    MEDIA_FORMAT_AAC_LC = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1060),        /**< media format mime type is AAC_LC,  AUDIO, (Since 2.4) */
+    MEDIA_FORMAT_AAC_HE = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1061),        /**< media format mime type is AAC_HE,  AUDIO, (Since 2.4) */
+    MEDIA_FORMAT_AAC_HE_PS = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1062),     /**< media format mime type is AAC_HE_PS,  AUDIO, (Since 2.4) */
     MEDIA_FORMAT_MP3 = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1070),           /**< media format mime type is MP3,  AUDIO*/
-    MEDIA_FORMAT_VORBIS = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1080),        /**< media format mime type is VORBIS,  AUDIO, (Since tizen 2.4) */
-    MEDIA_FORMAT_FLAC = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1090),          /**< media format mime type is FLAC,  AUDIO, (Since tizen 2.4) */
-    MEDIA_FORMAT_WMAV1 = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A0),         /**< media format mime type is WMAV1,  AUDIO, (Since tizen 2.4) */
-    MEDIA_FORMAT_WMAV2 = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A1),         /**< media format mime type is WMAV2,  AUDIO, (Since tizen 2.4) */
-    MEDIA_FORMAT_WMAPRO = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A2),        /**< media format mime type is WMAVPRO,  AUDIO, (Since tizen 2.4) */
-    MEDIA_FORMAT_WMALSL = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A3),        /**< media format mime type is WMAVLSL,  AUDIO, (Since tizen 2.4) */
+    MEDIA_FORMAT_VORBIS = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1080),        /**< media format mime type is VORBIS,  AUDIO, (Since 2.4) */
+    MEDIA_FORMAT_FLAC = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x1090),          /**< media format mime type is FLAC,  AUDIO, (Since 2.4) */
+    MEDIA_FORMAT_WMAV1 = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A0),         /**< media format mime type is WMAV1,  AUDIO, (Since 2.4) */
+    MEDIA_FORMAT_WMAV2 = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A1),         /**< media format mime type is WMAV2,  AUDIO, (Since 2.4) */
+    MEDIA_FORMAT_WMAPRO = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A2),        /**< media format mime type is WMAVPRO,  AUDIO, (Since 2.4) */
+    MEDIA_FORMAT_WMALSL = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_ENCODED | 0x10A3),        /**< media format mime type is WMAVLSL,  AUDIO, (Since 2.4) */
 
     MEDIA_FORMAT_PCM = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_RAW | 0x1510),               /**< media format mime type is PCM, AUDIO*/
     MEDIA_FORMAT_PCMA = (MEDIA_FORMAT_AUDIO | MEDIA_FORMAT_RAW | 0x1520),              /**< media format mime type is PCM A-law, AUDIO*/
@@ -126,10 +122,10 @@ typedef enum {
     MEDIA_FORMAT_MPEG2_HP = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x2062),      /**< media format mime type is MPEG2_HP, VIDEO */
     MEDIA_FORMAT_MPEG4_SP = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x2070),      /**< media format mime type is MPEG4_SP, VIDEO */
     MEDIA_FORMAT_MPEG4_ASP = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x2071),     /**< media format mime type is MPEG4_ASP, VIDEO */
-    MEDIA_FORMAT_HEVC = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x2080),          /**< media format mime type is HEVC, VIDEO, (Since tizen 2.4) */
-    MEDIA_FORMAT_VP8 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x2090),           /**< media format mime type is VP8, VIDEO, (Since tizen 2.4) */
-    MEDIA_FORMAT_VP9 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x20A0),           /**< media format mime type is VP9, VIDEO, (Since tizen 2.4) */
-    MEDIA_FORMAT_VC1 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x20B0),           /**< media format mime type is VC1, VIDEO, (Since tizen 2.4) */
+    MEDIA_FORMAT_HEVC = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x2080),          /**< media format mime type is HEVC, VIDEO, (Since 2.4) */
+    MEDIA_FORMAT_VP8 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x2090),           /**< media format mime type is VP8, VIDEO, (Since 2.4) */
+    MEDIA_FORMAT_VP9 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x20A0),           /**< media format mime type is VP9, VIDEO, (Since 2.4) */
+    MEDIA_FORMAT_VC1 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_ENCODED | 0x20B0),           /**< media format mime type is VC1, VIDEO, (Since 2.4) */
 
     MEDIA_FORMAT_I420 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_RAW | 0x2510),              /**< media format mime type is I420, VIDEO */
     MEDIA_FORMAT_NV12 = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_RAW | 0x2520),              /**< media format mime type is NV12, VIDEO */
@@ -146,23 +142,23 @@ typedef enum {
     MEDIA_FORMAT_ARGB = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_RAW | 0x25d0),              /**< media format mime type is ARGB, VIDEO */
 
     /* CONTAINER VIDEO */
-    MEDIA_FORMAT_CONTAINER_MP4 = (MEDIA_FORMAT_CONTAINER | 0x3010),
-    MEDIA_FORMAT_CONTAINER_AVI = (MEDIA_FORMAT_CONTAINER | 0x3020),
-    MEDIA_FORMAT_CONTAINER_MPEG2TS = (MEDIA_FORMAT_CONTAINER | 0x3030),
-    MEDIA_FORMAT_CONTAINER_MPEG2PS = (MEDIA_FORMAT_CONTAINER | 0x3040),
-    MEDIA_FORMAT_CONTAINER_MATROSKA = (MEDIA_FORMAT_CONTAINER | 0x3050),
-    MEDIA_FORMAT_CONTAINER_WEBM = (MEDIA_FORMAT_CONTAINER | 0x3060),
-    MEDIA_FORMAT_CONTAINER_3GP = (MEDIA_FORMAT_CONTAINER | 0x3070),
+    MEDIA_FORMAT_CONTAINER_MP4 = (MEDIA_FORMAT_CONTAINER | 0x3010),                    /**< media format mime type is MP4 container, VIDEO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_AVI = (MEDIA_FORMAT_CONTAINER | 0x3020),                    /**< media format mime type is AVI container, VIDEO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_MPEG2TS = (MEDIA_FORMAT_CONTAINER | 0x3030),                /**< media format mime type is MPEG2TS container, VIDEO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_MPEG2PS = (MEDIA_FORMAT_CONTAINER | 0x3040),                /**< media format mime type is MPEG2PS container, VIDEO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_MATROSKA = (MEDIA_FORMAT_CONTAINER | 0x3050),               /**< media format mime type is MATROSKA container, VIDEO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_WEBM = (MEDIA_FORMAT_CONTAINER | 0x3060),                   /**< media format mime type is WEBM container, VIDEO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_3GP = (MEDIA_FORMAT_CONTAINER | 0x3070),                    /**< media format mime type is 3GP container, VIDEO, (Since 3.0) */
 
     /*CONTAINER AUDIO */
-    MEDIA_FORMAT_CONTAINER_WAV = (MEDIA_FORMAT_CONTAINER | 0x4010),
-    MEDIA_FORMAT_CONTAINER_OGG = (MEDIA_FORMAT_CONTAINER | 0x4020),
-    MEDIA_FORMAT_CONTAINER_AAC_ADTS = (MEDIA_FORMAT_CONTAINER | 0x4030),
+    MEDIA_FORMAT_CONTAINER_WAV = (MEDIA_FORMAT_CONTAINER | 0x4010),                    /**< media format mime type is WAV container, AUDIO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_OGG = (MEDIA_FORMAT_CONTAINER | 0x4020),                    /**< media format mime type is OGG container, AUDIO, (Since 3.0) */
+    MEDIA_FORMAT_CONTAINER_AAC_ADTS = (MEDIA_FORMAT_CONTAINER | 0x4030),               /**< media format mime type is AAC_ADTS container, AUDIO, (Since 3.0) */
 
 
-    MEDIA_FORMAT_NATIVE_VIDEO = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_RAW | 0x7000),      /**< media format mime type is HW dependent , VIDEO, (Since tizen 2.4) */
+    MEDIA_FORMAT_NATIVE_VIDEO = (MEDIA_FORMAT_VIDEO | MEDIA_FORMAT_RAW | 0x7000),      /**< media format mime type is HW dependent , VIDEO, (Since 2.4) */
 
-    MEDIA_FORMAT_MAX          /**< media format mime type is MEDIA_FORMAT_MAX, Do not use */
+    MEDIA_FORMAT_MAX                                                                   /**< media format mime type is MEDIA_FORMAT_MAX, Do not use */
 } media_format_mimetype_e;
 
 /**
@@ -170,8 +166,8 @@ typedef enum {
  * @since_tizen 2.3
  */
 typedef enum {
-    MEDIA_COLOR_MODEL_NONE,          /**< media format color model is NONE */
-    MEDIA_COLOR_MODEL_RGB,          /**< media format color model is RGB */
+    MEDIA_COLOR_MODEL_NONE,        /**< media format color model is NONE */
+    MEDIA_COLOR_MODEL_RGB,         /**< media format color model is RGB */
     MEDIA_COLOR_MODEL_YUV          /**< media format color model is YUV */
 } media_format_color_model_e;
 
@@ -196,28 +192,27 @@ int media_format_create(media_format_h* fmt);
 
 /**
  * @brief Gets format type of media format
- * @since_tizen mmfw_internal
- * @details   Gets format type
- * @param[in] fmt media_format_h to get container mime type
- * @param[out] formattype media_format_type_e, ex) MEDIA_FORMAT_AUDIO
- * @retval #MEDIA_FORMAT_ERROR_NONE Successful
+ * @since_tizen 3.0
+ * @param[in] fmt The media_format_h to get format type
+ * @param[out] formattype The media_format_type_e, ex) MEDIA_FORMAT_AUDIO
  *
  * @return @c 0 on success,
  *         otherwise a negative error value
+ * @retval #MEDIA_FORMAT_ERROR_NONE Successful
  * @retval #MEDIA_FORMAT_ERROR_INVALID_PARAMETER Invalid parameter
  * @retval #MEDIA_FORMAT_ERROR_INVALID_OPERATION Invalid operation
  */
-int media_format_get_mime_format_type(media_format_h fmt, media_format_type_e* formattype);
+int media_format_get_type(media_format_h fmt, media_format_type_e* formattype);
 
 /**
  * @brief Gets container MIME type of media format
- * @since_tizen mmfw_internal
- * @details   Gets MIME type
- * @param[in] fmt media_format_h to get container mime type
- * @param[out] mimetype media_format_mimetype_e, ex) MEDIA_FORMAT_MP4
+ * @since_tizen 3.0
+ * @param[in] fmt The media_format_h to get container mime type
+ * @param[out] mimetype The media_format_mimetype_e, ex) MEDIA_FORMAT_CONTAINER_MP4
  *
  * @return @c 0 on success,
  *         otherwise a negative error value
+ * @retval #MEDIA_FORMAT_ERROR_NONE Successful
  * @retval #MEDIA_FORMAT_ERROR_NONE Successful
  * @retval #MEDIA_FORMAT_ERROR_INVALID_PARAMETER Invalid parameter
  * @see media_format_set_container_mime()
@@ -288,11 +283,10 @@ int media_format_get_audio_info(media_format_h fmt, media_format_mimetype_e* mim
 int media_format_get_audio_aac_type(media_format_h fmt, bool* is_adts);
 
 /**
- * @brief Gets video frame_rate of media format
- * @since_tizen mmfw_internal
- * @details   Gets frame_rate
- * @param[in] fmt media_format_h to get
- * @param[out] frame_rate frame rate of the video
+ * @brief Gets video frame rate of media format
+ * @since_tizen 3.0
+ * @param[in] fmt The media_format_h to get
+ * @param[out] frame_rate The video frame rate
  *
  * @return @c 0 on success,
  *         otherwise a negative error value
@@ -304,10 +298,9 @@ int media_format_get_video_frame_rate(media_format_h fmt, int* frame_rate);
 
 /**
  * @brief Sets container MIME type of media format
- * @since_tizen mmfw_internal
- * @details   Sets container MIME type
- * @param[in] fmt media_format_h to set
- * @param[in] mimetype media_format_mimetype_e, ex) MEDIA_FORMAT_MP4
+ * @since_tizen 3.0
+ * @param[in] fmt The media_format_h to set
+ * @param[in] mimetype The media_format_mimetype_e, ex) MEDIA_FORMAT_MP4
  *
  * @return @c 0 on success,
  *         otherwise a negative error value
@@ -404,11 +397,10 @@ int media_format_set_video_avg_bps(media_format_h fmt, int avg_bps);
 int media_format_set_video_max_bps(media_format_h fmt, int max_bps);
 
 /**
- * @brief Sets video frame_rate of media format
- * @since_tizen mmfw_internal
- * @details   Sets frame_rate
- * @param[in] fmt media_format_h to set
- * @param[in] frame_rate frame rate of the video
+ * @brief Sets video frame rate of media format
+ * @since_tizen 3.0
+ * @param[in] fmt The media_format_h to set
+ * @param[in] frame_rate The video frame rate
  * @pre must set video MIME type by media_format_set_video_mime()
  *
  * @return @c 0 on success,
@@ -598,4 +590,4 @@ int media_format_make_writable(media_format_h fmt, media_format_h* out_fmt);
 }
 #endif
 
-#endif /* __TIZEN_MEDIA_AV_PACKET_BUFFER_H__ */
+#endif /* __TIZEN_MEDIA_FORMAT_H__ */
